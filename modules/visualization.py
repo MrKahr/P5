d3512b67be651d01fb86dfdcaa27aa682a19d4e8
MrKahr <madska21@student.aau.dk>
2024-09-19 10:52:48 +0200

    Removed  unecessary files to greater project clarity

--- modules/visualization.py
+++ modules/visualization.py
@@ -6,11 +6,8 @@ plt.close("all")  # closes all currently active figures
 
 
 class AccuracyPlotter:
-    def __init__(self) -> None:
-        pass
-
-    def barPlot(self, x: ArrayLike | str, y1: ArrayLike, y2: ArrayLike) -> None:
+    def barPlot(self, label: list[str] | str, y1: ArrayLike, y2: ArrayLike) -> None:
         plt.figure()
-        plt.bar(x, y1, color="r")
-        plt.bar(x, y2, color="b")
+        plt.bar(label, y1, color="r")
+        plt.bar(label, y2, color="b")
         plt.show()
