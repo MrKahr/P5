af37844ca8f1cb9d337a3946ffbe38f3f481fded
nikarnik <112962573+nikarnik@users.noreply.github.com>
2024-09-27 11:32:13 +0200

    Plot histogram of outliers

--- modules/outlierAnalysis.py
+++ modules/outlierAnalysis.py
@@ -1,35 +1,26 @@
 from dataPreprocessing import DataProcessor, Dataset
 import pandas as pd
+import matplotlib.pyplot as plt
+import math
 
 
-
-def AVF(row, df):
-    # attributes = list(elem)
-    # print(type(row))
+def AVF(row):
     sum = 0
     keys = row.keys()
     i = 0
     for attributeVal in row:
-        # print(attribute)
-        # print("\n\n")
-        # print(df[0][0])
         sum += frequency(attributeVal, keys[i])
         i += 1
     return (1/len(row))*sum
     
 
-def frequency(value, attribute):
-    return ratio(attribute)[value]
+def frequency(value, attribute, frequencies={}):
+    # frequencies is only initialized once
+    if not frequencies.get(attribute):
+        frequencies[attribute] = ratio(attribute)
+    return frequencies[attribute][value]
     
 
-# for elem in df.iterrows():
-#     AVF(elem)
-
-# freqDict = dict()
-# columns = df.columns
-# for colName in columns:
-#     freqDict.update(colName)
-#     #print(df[colName])
 
 def ratio(attribute: str) -> dict:
     column = df[attribute]
@@ -43,19 +34,32 @@ def ratio(attribute: str) -> dict:
         sums[i] = sums[i] / len(column)
     return sums
 
-# print(df[0][0])
 
 # Process data
 dp = DataProcessor(Dataset.REGS)
 df = dp.dataFrame
 
+df.drop(["Gris ID", "Sår ID"], axis = 1, inplace = True)
+
 listAVF = []
 sum = 0
 for index, row in df.iterrows():
-    AVFelem = AVF(row, df)
+    AVFelem = AVF(row)
     listAVF.append(AVFelem)
     sum += AVFelem
 
 avg = sum / len(listAVF)
-print(avg)
+lowestPercentage = math.floor(len(listAVF) * 0.01)
+print("average:", avg)
+sortedList = sorted(listAVF)[:lowestPercentage]
+print("outliers:", sortedList)
 
+n, bins, patches = plt.hist(listAVF, bins=40)
+sumBars = 0
+i = 0
+while sumBars <= lowestPercentage:
+    patches[i].set_color("r")
+    sumBars += n[i]
+    i += 1
+
+plt.show()
