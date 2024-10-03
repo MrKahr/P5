4f385da9b33a48ef6f790adc0a85bd2bc4fbe86e
KristianEmilWN <92726828+KristianEmilWN@users.noreply.github.com>
2024-10-03 12:40:05 +0200

    main fix

--- main.py
+++ main.py
@@ -2,12 +2,10 @@
 from modules.dataPreprocessing import DataProcessor, Dataset
 from modules.models import GaussianNaiveBayes
 from modules.visualization import Plotter
-from modules.continuousVariables import ContinuousPlotter
 
 # Process data
 dp = DataProcessor(Dataset.REGS)
-condp = DataProcessor(Dataset.MÅL)
-condp.showDataFrame()
+
 # Model data
 model = GaussianNaiveBayes(dp.dataFrame)
 
@@ -22,9 +20,6 @@ model.generatePrediction(
 
 accplt = Plotter()
 xp, yp = model.getResults()
-#accplt.barPlot("Accuracy", xp, yp)
+accplt.barPlot("Accuracy", xp, yp)
 
-conplt = ContinuousPlotter()
-
-conplt.continousPlot(condp.dataFrame["Sårrand (cm)"], condp.dataFrame["Midte (cm)"], condp.dataFrame["Dag"])
 
