bcf9b9c170384bbf87ea40f7224f371f229f4769
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2024-10-10 15:23:54 +0200

    Testing

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 # Main entry-point into our code base
 from modules.dataPreprocessing.cleaner import DataCleaner
+from modules.dataPreprocessing.feature_selector import FeatureSelector
 from modules.dataPreprocessing.preprocessor import DataPreprocessor, Dataset
 
 
 dp = DataPreprocessor(Dataset.REGS)
 cleaner = DataCleaner(dp.df)
 cleaner.cleanRegsDataset()
-cleaner.deleteMissingValue()
-cleaner.showRowRemovalRatio()
+cleaner.deleteNonfeatures()
+dp.df = cleaner.getDataframe()
+fs = FeatureSelector(None, None)
+
+X, y = dp.getTrainingData(), dp.getTargetData()
+x_labels = dp.getTrainingLabels()
+fs.genericUnivariateSelect(
+    X=X,
+    y=y,
+    scoreFunc=fs._mutualInfoClassif,
+    mode="percentile",
+    param=50,
+    x_labels=x_labels,
+)
