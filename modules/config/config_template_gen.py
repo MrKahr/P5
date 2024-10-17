26bc77ee3bdaa4c5eea27971968fbc99cc4a9b0b
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2024-10-17 13:29:59 +0200

    Added decisiontree and random forest to config

--- modules/config/config_template_gen.py
+++ modules/config/config_template_gen.py
@@ -1,5 +1,7 @@
 from typing import Self
 
+from modules.config.config_enums import Model
+
 
 class ConfigTemplate(object):
     """Singleton that defines as configuration template for the project.
@@ -25,7 +27,29 @@ class ConfigTemplate(object):
                 "loglevel": "DEBUG",
             },
             "DataPreprocessing": {"test1": ""},
-            "ModelSelection": {"test2": ""},
+            "ModelSelection": {
+                "model": Model.DECISION_TREE.name,
+                "DecisionTree": {
+                    "criterion": "entropy",  # type: Literal["gini", "entropy", "log_loss"]
+                    "max_depth": None,  # type: int
+                    "min_samples_split": 2,
+                    "min_samples_leaf": 1,
+                    "min_weight_fraction_leaf": 0,  # type: int | float
+                    "max_features": None,  # type: int
+                    "random_state": 42,
+                    "max_leaf_nodes": None,  # type: int
+                    "min_impurity_decrease": 0.0,
+                    "ccp_alpha": 0.0,
+                },
+                "RandomForest": {
+                    "n_estimators": 100,
+                    "bootstrap": True,
+                    "oob_score": False,  # type: bool | Callable # TODO: Add score function
+                    "n_jobs": -1,
+                    "random_state": 53,
+                    "max_samples": None,  # type: int | float
+                },
+            },
             "ModelTraining": {"test3": ""},
             "ModelTesting": {"test4": ""},
             "ModelEvaluation": {"test5": ""},
