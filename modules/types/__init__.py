71ce465d291ef8eae1d6019022044b268675471e
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2024-10-23 16:17:41 +0200

    Add custom type aliases

--- modules/types/__init__.py
+++ modules/types/__init__.py
@@ -1,10 +1,11 @@
 import os
-from typing import TypeAlias, Union
+from typing import Callable, TypeAlias, Union
+
+from numpy.typing import NDArray
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import StratifiedKFold, TimeSeriesSplit
 from sklearn.naive_bayes import GaussianNB
-from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 
 
@@ -12,10 +13,14 @@ StrPath: TypeAlias = str | os.PathLike[str]
 
 
 UnfittedEstimator: TypeAlias = Union[
-    DecisionTreeClassifier, RandomForestClassifier, GaussianNB, SVC
+    DecisionTreeClassifier, RandomForestClassifier, GaussianNB
 ]
 FittedEstimator: TypeAlias = Union[
-    DecisionTreeClassifier, RandomForestClassifier, GaussianNB, SVC
+    DecisionTreeClassifier, RandomForestClassifier, GaussianNB
 ]
 
 CrossValidator: TypeAlias = Union[StratifiedKFold, TimeSeriesSplit]
+
+ScoreCallable: TypeAlias = Callable[
+    [NDArray, NDArray], tuple[NDArray, NDArray] | NDArray
+]
