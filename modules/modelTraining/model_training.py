2c4f6a1d3c8b801a9d33549dd1edecba2f4eb4c5
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2024-10-23 16:18:36 +0200

    Supposedly MVP model trainer. Testing needed

--- modules/modelTraining/model_training.py
+++ modules/modelTraining/model_training.py
@@ -1,5 +1,5 @@
 from time import time
-from typing import Any, Callable, TypeAlias, Union
+from typing import Any, Callable
 from numpy.typing import NDArray
 from sklearn.feature_selection import RFE, RFECV
 from sklearn.inspection import permutation_importance
@@ -8,15 +8,46 @@ from sklearn.utils import Bunch
 
 from modules.config.config import Config
 from modules.config.config_enums import TrainingMethod
+from modules.crossValidationSelection.cross_validation_selection import (
+    CrossValidationSelector,
+)
 from modules.logging import logger
-
-
+from modules.types import (
+    FittedEstimator,
+    UnfittedEstimator,
+    CrossValidator,
+    ScoreCallable,
+)
 
 
 class ModelTrainer:
     _logger = logger
 
-    def __init__(self) -> None:
+    def __init__(
+        self,
+        estimator: UnfittedEstimator,
+        train_x: NDArray,
+        target_y: NDArray,
+    ) -> None:
+        """Fit an unfitted model and generate a model report of the training session.
+
+        Parameters
+        ----------
+        estimator : UnfittedEstimator
+            The unfitted estimator to fit/train.
+
+        train_x : NDArray
+            Training data.
+
+        target_y : NDArray
+            Target data.
+        """
+        self._unfit_estimator = estimator
+        self._train_x = train_x
+        self._target_y = target_y
+        self._config = Config()
+        self._parent_key = "ModelTraining"
+        self._n_jobs = self._config.getValue("n_jobs", "General")
         self._model_report = {
             "feature_importances": None,  # type: Bunch
             "feature_names_in": None,  # type: NDArray
@@ -30,8 +61,6 @@ class ModelTrainer:
             "recall": None,  # type: float
             "specificity": None,  # type: float
         }
-        self._config = Config()
-        self._parent_key = "ModelTraining"
 
     def _checkAllFeaturesPresent(self) -> None:
         key = "GenericUnivariateSelect"
@@ -42,22 +71,18 @@ class ModelTrainer:
                 + f"Please disable '{key}' (within '{parent_key}') in the config when tuning hyperparameters"
             )
 
-    def _compileModelReport(
-        self, fitted_estimator: Any, train_x: NDArray, target_y: NDArray
-    ) -> None:
+    def _compileModelReport(self, estimator: FittedEstimator) -> None:
         self._model_report["feature_importances"] = self._permutationFeatureImportance(
-            fitted_estimator,
-            train_x,
-            target_y,
-            self._config.getValue(
+            estimator,
+            **self._config.getValue(
                 "PermutationFeatureImportance", parent_key=self._parent_key
             ),
         )
-        self._model_report["feature_names_in"] = fitted_estimator.feature_names_in_
-        self._model_report["feature_count"] = fitted_estimator.n_features_in_
+        self._model_report["feature_names_in"] = estimator.feature_names_in_
+        self._model_report["feature_count"] = estimator.n_features_in_
 
     def _permutationFeatureImportance(
-        self, fitted_estimator: Any, train_x: NDArray, target_y: NDArray, **kwargs
+        self, estimator: FittedEstimator, **kwargs
     ) -> Bunch:
         """
         Permutation feature importance is a model inspection technique
@@ -75,14 +100,8 @@ class ModelTrainer:
 
         Parameters
         ----------
-        fitted_estimator : Any
+        estimator : FittedEstimator
             A fitted estimator.
-
-        X : NDArray
-            Training data.
-
-        y : NDArray
-            Target data.
 
         **kwargs : dict
             Additional parameters defined in the config.
@@ -111,10 +130,10 @@ class ModelTrainer:
         self._logger.info("Computing permutation feature importances...")
         start_time = time()
         result = permutation_importance(
-            fitted_estimator,
-            train_x,
-            target_y,
-            n_jobs=-1,
+            estimator,
+            self._train_x,
+            self._target_y,
+            n_jobs=self._n_jobs,
             **kwargs,
         )
         self._logger.info(
@@ -122,51 +141,43 @@ class ModelTrainer:
         )
         return result
 
-    def _fitGridSearchCV(
-        self, unfit_estimator: Any, train_x: NDArray, target_y: NDArray, **kwargs
-    ) -> FitEstimator:
+    def _fitGridSearchCV(self, **kwargs) -> FittedEstimator:
         self._checkAllFeaturesPresent()
         gscv = GridSearchCV()
 
-    def _fitRandomSearchCV(
-        self, unfit_estimator: Any, train_x: NDArray, target_y: NDArray, **kwargs
-    ) -> FitEstimator:
+    def _fitRandomSearchCV(self, **kwargs) -> FittedEstimator:
         self._checkAllFeaturesPresent()
         rscv = RandomizedSearchCV()
 
     def _fitRFECV(
-        self, unfit_estimator: Any, train_x: NDArray, target_y: NDArray, **kwargs
-    ) -> FitEstimator:
+        self, cv: CrossValidator, scoring: ScoreCallable, **kwargs
+    ) -> FittedEstimator:
         self._checkAllFeaturesPresent()
-        rfecv = RFECV()
+        rfecv = RFECV(
+            self._unfit_estimator, cv=cv, scoring=scoring, n_jobs=self._n_jobs, **kwargs
+        )
 
-    def _fitRFE(
-        self, unfit_estimator: Any, train_x: NDArray, target_y: NDArray, **kwargs
-    ) -> FitEstimator:
+    def _fitRFE(self, **kwargs) -> FittedEstimator:
         self._checkAllFeaturesPresent()
         self._logger.info(f"Fitting model...")
         start_time = time()
-        rfe = RFE(unfit_estimator, **kwargs).fit(train_x, target_y)
+        rfe = RFE(self._unfit_estimator, **kwargs).fit(self._train_x, self._target_y)
         self._logger.info(f"Model fitted in {time()-start_time:.3f} s")
-        self._compileModelReport(rfe, train_x, target_y)
+        self._compileModelReport(rfe, self._train_x, self._target_y)
         return rfe.estimator_
 
     def _fitCV(
         self,
-        unfit_estimator: Any,
-        cv: Any,
-        train_x: NDArray,
-        target_y: NDArray,
+        cv: CrossValidator,
         scoring: Callable,
-    ) -> FitEstimator:
+    ) -> FittedEstimator:
         cv_results = cross_validate(
-            estimator=unfit_estimator,
-            X=train_x,
-            y=target_y,
-            groups=None,
+            estimator=self._unfit_estimator,
+            X=self._train_x,
+            y=self._target_y,
             scoring=scoring,
             cv=cv,
-            n_jobs=-1,
+            n_jobs=self._n_jobs,
             return_estimator=True,
         )
         ft = cv_results["estimator"]
@@ -174,15 +185,12 @@ class ModelTrainer:
 
     def _fit(
         self,
-        unfit_estimator: Any,
-        train_x: NDArray,
-        target_y: NDArray,
-    ) -> FitEstimator:
+    ) -> FittedEstimator:
         self._logger.info(f"Fitting model...")
         start_time = time()
-        fitted_estimator = unfit_estimator.fit(train_x, target_y)
+        fitted_estimator = self._unfit_estimator.fit(self._train_x, self._target_y)
         self._logger.info(f"Model fitted in {time()-start_time:.3f} s")
-        self._compileModelReport(fitted_estimator, train_x, target_y)
+        self._compileModelReport(fitted_estimator)
         return fitted_estimator
 
     def getModelReport(self) -> dict[str, Any]:
@@ -191,42 +199,35 @@ class ModelTrainer:
     # TODO: train_x and target_y MUST include labels (for ALL methods using them)
     def run(
         self,
-        unfit_estimator: Any,
-        train_x: NDArray,
-        target_y: NDArray,
-    ) -> Union[]:
+    ) -> FittedEstimator:
         start_time = time()
         training_method = self._config.getValue(
             "training_method", parent_key=self._parent_key
         )
+        cv = CrossValidationSelector().getCrossValidator()
+        scoring = "accuracy"  # TODO: getScoring()
 
         if training_method == TrainingMethod.FIT:
-            fitted_estimator = self._fit(
-                unfit_estimator=unfit_estimator, train_x=train_x, target_y=target_y
-            )
+            fitted_estimator = self._fit()
         elif training_method == TrainingMethod.CROSS_VALIDATION:
             fitted_estimator = self._fitCV(
-                unfit_estimator=unfit_estimator,
-                train_x=train_x,
-                target_y=target_y,
-                scoring=getScoring(),
+                cv=cv,
+                scoring=scoring,
             )
         elif training_method == TrainingMethod.RFE:
             fitted_estimator = self._fitRFE(
-                unfit_estimator=unfit_estimator, train_x=train_x, target_y=target_y
+                **self._config.getValue("RFE", self._parent_key)
             )
         elif training_method == TrainingMethod.RFECV:
             fitted_estimator = self._fitRFECV(
-                unfit_estimator=unfit_estimator, train_x=train_x, target_y=target_y
+                cv=cv,
+                scoring=scoring,
+                **self._config.getValue("RFECV", self._parent_key),
             )
         elif training_method == TrainingMethod.RANDOM_SEARCH_CV:
-            fitted_estimator = self._fitRandomSearchCV(
-                unfit_estimator=unfit_estimator, train_x=train_x, target_y=target_y
-            )
+            fitted_estimator = self._fitRandomSearchCV()
         elif training_method == TrainingMethod.GRID_SEARCH_CV:
-            fitted_estimator = self._fitGridSearchCV(
-                unfit_estimator=unfit_estimator, train_x=train_x, target_y=target_y
-            )
+            fitted_estimator = self._fitGridSearchCV()
         else:
             raise TypeError(
                 f"Invalid training method '{training_method}'. Expected one of {TrainingMethod._member_names_}"
