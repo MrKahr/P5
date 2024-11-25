d975c985a4f5618335734893cc84b4c76bc807b0
LouiseWolff17 <112960420+LouiseWolff17@users.noreply.github.com>
2024-11-25 14:44:57 +0100

    whatever

--- modules/modelSummary/model_summarizing.py
+++ modules/modelSummary/model_summarizing.py
@@ -195,8 +195,9 @@ class ModelSummary:
     def plotFeatureImportance(self) -> None:
         
         # Get the feature importances(importances_mean, importances_std, importances) and feature names from the model report
-        result = self._model_report.get("feature_importances").get("threshold")
+        result = self._model_report["feature_importances"].get("threshold")
         feature_names = self._model_report["feature_names_in"]
+        print(result)
         
         # Label the means with their feature name
         feature_importances = pd.Series(result.importances_mean,index=feature_names)
