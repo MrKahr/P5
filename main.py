776d851441092d87e8efa00ee81124851ed3466b
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2024-11-28 15:08:55 +0100

    Implement CLI

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
-##########################
-### Initial Path Setup ###
-##########################
+#####################
+### Initial Setup ###
+#####################
 # Set initial CWD
 import os
 
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-from modules.logging.logger import Logger
 
+from modules.logging.logger import Logger
+from modules.tools.arguments.app_arguments import AppArguments
+
+arguments = AppArguments()
 Logger.writeHeaderToLog()  # Prevent multi-threading bug with startup header
+arguments.executeArguments()
+
 ######################
 ### Module Imports ###
 ######################
+from modules.config.utils.setup_config import SetupConfig
 from modules.dataPreprocessing.dataset_enums import Dataset
 from modules.pipeline import Pipeline
 
-pipeline = Pipeline(Dataset.REGS)
-pipeline.run()
+
+if SetupConfig.arg_batch:
+    pass
+else:
+    Pipeline(Dataset.REGS).run()
