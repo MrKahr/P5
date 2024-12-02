63feb46d4bc1270f70d01ad7fd0c1fe0fa6836f3
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2024-12-02 14:03:23 +0100

    Implement batch pipeline

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 ### Initial Setup ###
 #####################
 import os
+from tqdm import tqdm
 
 # Set initial CWD
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -18,10 +19,16 @@ arguments.executeArguments()
 from modules.config.utils.setup_config import SetupConfig
 from modules.dataPreprocessing.dataset_enums import Dataset
 from modules.pipeline import Pipeline
-
+from modules.config.utils.config_batch_processor import ConfigBatchProcessor
+from modules.logging import logger
 
 if SetupConfig.arg_batch:
-
-    pass
+    config_list = ConfigBatchProcessor.getConfigPairsFromBatch(
+        ConfigBatchProcessor.getBatchConfigs(SetupConfig.config_dir)
+    )
+    logger.info(f"Running in batch mode. Processing {len(config_list)} experiments")
+    for configs in tqdm(config_list):
+        ConfigBatchProcessor.applyConfigs(configs)
+        Pipeline(Dataset.REGS).run()
 else:
     Pipeline(Dataset.REGS).run()
---
+++ modules/config/utils/config_batch_processor.py
@@ -0,0 +1,80 @@
+from copy import deepcopy
+import glob
+import os
+from typing import Any, Generator
+from modules.config.utils.setup_config import SetupConfig
+from modules.logging import logger
+from modules.tools.types import StrPath
+
+
+class ConfigBatchProcessor:
+    _logger = logger
+
+    @classmethod
+    def getBatchConfigs(
+        cls, folder: StrPath, extensions: list[str] = ["json"]
+    ) -> list[StrPath]:
+        """
+        Recursively find all files matching an extension in `extensions`.
+
+        Parameters
+        ----------
+        folder : StrPath
+            The directory which to search in.
+
+        extensions : list[str], optional
+            Match files with these extensions.
+            By default `["json"]`.
+
+        Returns
+        -------
+        list[StrPath]
+            A sorted list of files matching `extensions` found in `folder`.
+        """
+        files = []
+        files.extend(
+            [glob.glob(f"{folder}/**/*.{extension}", recursive=True)]
+            for extension in extensions
+        )
+        return sorted(files)
+
+    @classmethod
+    def getConfigPairsFromBatch(
+        cls,
+        configs: list[StrPath],
+    ) -> Generator[list[StrPath], Any, None]:
+        """
+        Get a list of matching configs for use in a pipeline.
+
+        Parameters
+        ----------
+        configs : list[StrPath]
+            Search for matching configs in this list.
+
+        Yields
+        ------
+        Generator[list[StrPath], Any, None]
+            A list of macthing configs.
+        """
+        while configs:
+            config_pair = [configs.pop()]
+            current_file_id = config_pair[0].split(".")[2]
+            for config in deepcopy(configs):
+                if config.split(".")[2] == current_file_id:
+                    config_pair.append(configs.pop())
+            yield config_pair
+
+    @classmethod
+    def applyConfigs(cls, configs: list[StrPath]) -> None:
+        for config in configs:
+            file = os.path.split(config)[1]
+            if config.find("gridparams") != -1:
+                SetupConfig.grid_config_file = file
+                SetupConfig.grid_config_path = config
+            elif config.find("pipeline_config") != 1:
+                SetupConfig.pipeline_config_file = file
+                SetupConfig.pipeline_config_path = config
+            else:
+                cls._logger.error(
+                    f"Unrecognized config '{config}'. It is most likely missing from '{SetupConfig.__name__}'"
+                )
