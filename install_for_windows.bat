02cd5c21d53c46fa69fe34d1e20a31dbc2e811ff
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2024-12-16 14:10:12 +0100

    Improve script logic and bugfixing

--- install_for_windows.bat
+++ install_for_windows.bat
@@ -1,11 +1,9 @@
 @echo off
 CD /D "%~dp0"
 
-::Runners to simulate function calls
-SET call_condainstaller=0
-SET call_condaenviron=0
-SET call_cudapath=0
-SET call_torchpatch=0
+:: Installer
+SET installer_path=installer
+SET del_conda_cmd=%~dp0%installer_path%\delconda.bat.lnk
 
 :: Conda
 SET conda_name=Miniconda3
@@ -15,7 +13,13 @@ SET conda_env_name=.conda
 SET conda_env_prefix=./%conda_env_name%
 SET conda_env_path=%~dp0%conda_env_name%
 SET conda_environ=environment.yml
-SET conda_path=%USERPROFILE%\miniconda3\_conda.exe
+SET conda_path=%USERPROFILE%\Miniconda3
+SET conda_exe=%conda_path\_conda.exe
+SET conda_shell_hook=%USERPROFILE%\miniconda3\shell\condabin\conda-hook.ps1
+
+:: Python & Program
+SET conda_python_path=%conda_env_path%\python.exe
+SET main_path=%~dp0main.py
 
 ::Triton Windows
 SET triton_windows_name=Triton Windows
@@ -28,7 +32,7 @@ SET expected_cudalib_path=%cudalib_basepath%\x64\%cudalib_file%
 SET torch_name=PyTorch
 SET torch_codecache_file=codecache.py
 SET torch_codecache_path=".conda\Lib\site-packages\torch\_inductor\%torch_codecache_file%"
-SET torch_patched_codecache_path=installer\%torch_codecache_file%
+SET torch_patched_codecache_path=%installer_path%\%torch_codecache_file%
 
 :intro
     echo:
@@ -54,71 +58,62 @@ SET torch_patched_codecache_path=installer\%torch_codecache_file%
 
 :menu
     echo:
+    echo:
+    echo:
     echo: ____Start______________
-    echo:  1. Install everything
+    echo:  1. Install everything.
+    echo:  2. Start program.
     echo:
     echo: ____%conda_name%___________
-    echo:  c1. Download and install %conda_name%
-    echo:  c2. Create %conda_name% virtual environment
-    echo:      Expected %conda_name% location: '%conda_path%'
+    echo:  c1. Download and install %conda_name%.
+    echo:  c2. Create %conda_name% virtual environment.
+    echo:      Expected %conda_name% location: '%conda_exe%'.
     echo:
     echo: ____%triton_windows_name%______________
-    echo:  t1. Copy '%actual_cudalib_path%' to '%expected_cudalib_path%'
+    echo:  t1. Copy '%actual_cudalib_path%' to '%expected_cudalib_path%'.
     echo:  h1. Explain why this is required.
     echo:
     echo: ____%torch_name%______________
-    echo:  p1. Patch bug in file %torch_codecache_path% using file '%torch_patched_codecache_path%'
+    echo:  p1. Patch bug in file %torch_codecache_path% using file '%torch_patched_codecache_path%'.
     echo:  h2. Explain why this is required.
     echo:
     echo: ____Other______________
-    echo:  o1. Reinstall environment
+    echo:  o1. Reinstall environment.
     echo:
     set /p menuChoice="Select option: "
 
     IF %menuChoice%==1 (
-        SET call_condainstaller=1
-        SET call_condaenviron=1
-        SET call_cudapath=1
-        SET call_torchpatch=1
-        GOTO run
+        CALL :install_conda
+        CALL :install_dependencies
+        CALL :copy_files_to_cuda_path
+        CALL :fix_bug_in_pytorch
+        ECHO:
+        ECHO: Everything is ready!
+    )
+    IF %menuChoice%==2 (
+        CALL :start_program
     )
     IF %menuChoice%==c1 (
-        SET call_condainstaller=1
-        SET call_condaenviron=0
-        SET call_cudapath=0
-        SET call_torchpatch=0
-        GOTO run
+        CALL :install_conda
     )
     IF %menuChoice%==c2 (
-        SET call_condainstaller=0
-        SET call_condaenviron=1
-        SET call_cudapath=0
-        SET call_torchpatch=0
-        GOTO run
+        CALL :install_dependencies
     )
     IF %menuChoice%==t1 (
-        SET call_condainstaller=0
-        SET call_condaenviron=0
-        SET call_cudapath=1
-        SET call_torchpatch=0
-        GOTO run
+        CALL :copy_files_to_cuda_path
     )
     IF %menuChoice%==p1 (
-        SET call_condainstaller=0
-        SET call_condaenviron=0
-        SET call_cudapath=0
-        SET call_torchpatch=1
-        GOTO run
+        CALL :fix_bug_in_pytorch
     )
     IF %menuChoice%==o1 (
-        ECHO:
-        ECHO: Deleting '%conda_env_path%'
-        DEL /S /Q %conda_env_path%
-        SET call_condainstaller=1
-        SET call_condaenviron=1
-        SET call_cudapath=1
-        SET call_torchpatch=1
-        GOTO run
+        IF EXIST "%conda_env_path%" (
+            ECHO:
+            ECHO: Deleting '%conda_env_path%'
+            RD /S /Q "%conda_env_path%"
+        )
+        CALL :install_dependencies
+        CALL :copy_files_to_cuda_path
+        CALL :fix_bug_in_pytorch
     )
     IF %menuChoice%==h1 (
         ECHO:
@@ -150,63 +145,58 @@ SET torch_patched_codecache_path=installer\%torch_codecache_file%
     GOTO menu
 
 
-:: Function entry-point
-:run
+:install_conda
+ECHO: Downloading %conda_url%
+curl %conda_url% -o miniconda.exe
+ECHO: Installing %conda_name%...
+START /wait "" .\miniconda.exe /S
+DEL miniconda.exe
+ECHO: %conda_name% installed sucessfully!
+EXIT /B
 
-
-:install-conda
-IF %call_condainstaller%==1 (
-    ECHO: Downloading %conda_url%
-    curl %conda_url% -o miniconda.exe
-    ECHO: Installing %conda_name%...
-    START /wait "" .\miniconda.exe /S
-    DEL miniconda.exe
-    ECHO: Done
+:install_dependencies
+IF NOT EXIST "%conda_exe%" (
+    ECHO:
+    ECHO: Cannot find path '%conda_exe%'.
+    ECHO: Please specify %conda_name% install path.
+    SET /p conda_exe="Enter conda path: "
+    GOTO install_dependencies
+) ELSE (
+    ECHO:
+    ECHO: Creating environment at '%conda_env_path%'
+    ECHO: ^(This will take a while and it may seem stuck^)
+    ECHO:
+    ECHO:
+    "%conda_exe%" env create --prefix %conda_env_prefix% -f %conda_environ%
 )
+EXIT /B
 
-:install-dependencies
-IF %call_condaenviron%==1 (
-    IF NOT EXIST "%conda_path%" (
-        ECHO:
-        ECHO: Cannot find path '%conda_path%'.
-        ECHO: Please specify %conda_name% install path.
-        SET /p conda_path="Enter conda path: "
-        GOTO install-dependencies
-    ) ELSE (
-        ECHO:
-        ECHO: Creating environment at '%conda_env_path%'...
-        "%conda_path%" env create --prefix %conda_env_prefix% -f %conda_environ%
-        ECHO:
-        ECHO: Activating environment
-        "%conda_path%" activate %conda_env_path%
-        ECHO: Done
-    )
-)
-
-:copy-files-to-cuda-path
+:copy_files_to_cuda_path
 :: Triton Windows has hardcoded its expected CUDA_PATH of a specific file ("lib" / "x64" / "cuda.lib")
 :: which uses the location provided by the GUI installer (https://developer.nvidia.com/cuda-downloads)
 :: However, with conda, CUDA_PATH of this file is ("lib" / "cuda.lib")
 :: Thus, we copy the file to the CUDA_PATH Triton Windows expects.
-IF %call_cudapath%==1 (
-    ECHO: Copying '%actual_cudalib_path%' to '%expected_cudalib_path%'
-    COPY %actual_cudalib_path% %expected_cudalib_path%
-    ECHO: Done
-)
+ECHO: Copying '%actual_cudalib_path%' to '%expected_cudalib_path%'
+COPY %actual_cudalib_path% %expected_cudalib_path%
+EXIT /B
 
-:fix-bug-in-PyTorch
+:fix_bug_in_pytorch
 :: Windows-specific path-rename bug making our program explode.
 :: See:
 ::  https://github.com/pytorch/pytorch/issues/138211
 ::  https://github.com/woct0rdho/triton-windows/issues/10
 :: Issue is resolved by PyTorch in v2.6 nightly, however, we use 2.5.1 stable.
 :: Thus, we fix the bug manually using the code from the fixed PyTorch version.
-IF %call_torchpatch%==1 (
-    ECHO: Patching file '%torch_codecache_path%'
-    DEL %torch_codecache_path%
-    COPY %torch_patched_codecache_path%  %torch_codecache_path%
-    ECHO: Done
-)
+ECHO: Patching file '%torch_codecache_path%'
+DEL %torch_codecache_path%
+COPY %torch_patched_codecache_path%  %torch_codecache_path%
+EXIT /B
 
-
-GOTO menu
+:start_program
+ECHO:
+ECHO: Activating environment
+CALL powershell -ExecutionPolicy ByPass -NoExit -Command "& '%conda_shell_hook%'; conda activate '%conda_env_path%'; '%conda_python_path%' '%main_path%'; Write-Host '`n'; Read-Host -Prompt 'Press Enter to exit'"
+pause
+ECHO: Starting program
+START powershell -NoExit -command "& '%conda_exe%' activate '%conda_env_path%'"; -command "'%conda_python_path%' '%main_path%'"; Write-Host "`n"; "Read-Host -Prompt 'Press Enter to exit'"
+EXIT /B
